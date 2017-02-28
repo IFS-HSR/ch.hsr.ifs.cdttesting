1cdf56752ebba5924a1db0d41c20b61eec0c8271
Tobias Stauber <tobias.stauber@hsr.ch>
2017-02-28 13:29:36 +0100

    Tests for Normalizer and maven integration

---
+++ ch.hsr.ifs.cdttesting.test/src/ch/hsr/ifs/cdttesting/test/TestFileEditorTests.java
@@ -0,0 +1,16 @@
+package ch.hsr.ifs.cdttesting.test;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+import ch.hsr.ifs.cdttesting.test.cdttest.tests.NormalizerTest;
+
+@RunWith(Suite.class)
+//@formatter:off
+@SuiteClasses({
+	NormalizerTest.class
+	})
+public class TestFileEditorTests {
+
+}
---
+++ ch.hsr.ifs.cdttesting.test/src/ch/hsr/ifs/cdttesting/test/cdttest/tests/NormalizerTest.java
@@ -0,0 +1,101 @@
+package ch.hsr.ifs.cdttesting.test.cdttest.tests;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import ch.hsr.ifs.cdttesting.cdttest.CDTSourceFileTest;
+
+/**
+ * @author tstauber
+ *
+ *         Tests for RTS normalizer to strip formatting differences
+ *
+ */
+public class NormalizerTest {
+
+	@Test
+	public void leadingLineBreaks() {
+		//@formatter:off
+		final String input = "\n\n\n#include <cstdint>";
+
+		final String should = "#include <cstdint>";
+		//@formatter:on
+
+		assertEquals(should, CDTSourceFileTest.normalize(input));
+	}
+
+	@Test
+	public void trailingLineBreaks() {
+		//@formatter:off
+		final String input = "#include <cstdint>\n\n\n";
+
+		final String should = "#include <cstdint>";
+		//@formatter:on
+
+		assertEquals(should, CDTSourceFileTest.normalize(input));
+	}
+
+	@Test
+	public void shortenMultipleSpaces() {
+		//@formatter:off
+		final String input = "int      foo   {42};";
+
+		final String should = "int foo {42};";
+		//@formatter:on
+
+		assertEquals(should, CDTSourceFileTest.normalize(input));
+	}
+
+	@Test
+	public void removeLeadingSpaces() {
+		//@formatter:off
+		final String input = "    int foo {42};";
+
+		final String should = "int foo {42};";
+		//@formatter:on
+
+		assertEquals(should, CDTSourceFileTest.normalize(input));
+	}
+
+	@Test
+	public void removeTrailingSpaces() {
+		//@formatter:off
+		final String input = "int foo {42};    ";
+
+		final String should = "int foo {42};";
+		//@formatter:on
+
+		assertEquals(should, CDTSourceFileTest.normalize(input));
+	}
+
+	@Test
+	public void replaceLineBreaks() {
+		//@formatter:off
+		final String input = "#include <cstdint>\n"+
+							 "int main() {\r\n" +
+							 "  int foo = 42;\n" +
+							 "}";
+
+		final String should = "#include <cstdint>↵"+
+				 			  "int main() {↵" +
+				 			  "int foo = 42;↵" +
+				 			  "}";
+		//@formatter:on
+
+		assertEquals(should, CDTSourceFileTest.normalize(input));
+	}
+
+	@Test
+	public void removeRTSComments() {
+		//@formatter:off
+		final String input = "#include <cstdint>\n" +
+							 "/*TODO write more tests */";
+
+		final String should = "#include <cstdint>";
+		//@formatter:on
+
+		assertEquals(should, CDTSourceFileTest.normalize(input));
+	}
+
+}
